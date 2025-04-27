e1b84e8853596afbf43fd2fe1d2298f6159ffa3e
nbacadlabs <nbacadlabs@nexgbits.com>
2025-04-27 14:15:57 -0400

    updated

--- vpc/main.tf
+++ vpc/main.tf
@@ -5,7 +5,7 @@ locals {
 terraform {
   backend "s3" {
     bucket         = "nbmy-terraform-state-bucket-12345"
-    key            = "vpc/dev/terraform.tfstate"
+    key            = "./bootstrap/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
     encrypt        = true
