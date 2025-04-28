8fb5b29a42231f9dac05ee5e6f0ebccaef25b5f3
nbacadlabs <nbacadlabs@nexgbits.com>
2025-04-27 20:01:17 -0400

    updated all

--- aksbootstrap/ak-backend.tf
+++ aksbootstrap/ak-backend.tf
@@ -4,7 +4,7 @@ resource "azurerm_resource_group" "backend_rg" {
 }
 
 resource "azurerm_storage_account" "backend_sa" {
-  name                     = "tfbackendstorage123" # must be globally unique
+  name                     = "nbtfbackendstorage123" # must be globally unique
   resource_group_name      = azurerm_resource_group.backend_rg.name
   location                 = azurerm_resource_group.backend_rg.location
   account_tier             = "Standard"
